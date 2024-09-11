fae0ffca71e12e93c2fcadfcb64ed8e845b76bb5
zehao-intel <zehao.huang@intel.com>
2024-09-11 15:55:22 +0800

    add explanatory note on why multiply 8
    Signed-off-by: zehao-intel <zehao.huang@intel.com>

--- vllm/worker/habana_model_runner.py
+++ vllm/worker/habana_model_runner.py
@@ -1471,6 +1471,8 @@ class HabanaModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
             cache_size_limit = len(self.prompt_buckets) + len(self.decode_buckets)
             torch._dynamo.config.cache_size_limit = max(cache_size_limit, 
                                                         torch._dynamo.config.cache_size_limit)
+            # Multiply by 8 to follow the original default ratio between
+            # the cache_size_limit and accumulated_cache_size_limit
             torch._dynamo.config.accumulated_cache_size_limit = max(cache_size_limit * 8, 
                                                                     torch._dynamo.config.accumulated_cache_size_limit)
         
