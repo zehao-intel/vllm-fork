7a82b7382a73715878a5aa1354d509fc89397bb0
zehao-intel <zehao.huang@intel.com>
2024-09-11 15:34:46 +0800

    select bigger value
    Signed-off-by: zehao-intel <zehao.huang@intel.com>

--- vllm/worker/habana_model_runner.py
+++ vllm/worker/habana_model_runner.py
@@ -1469,8 +1469,10 @@ class HabanaModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
 
         if not htorch.utils.internal.is_lazy() and not self.enforce_eager:
             cache_size_limit = len(self.prompt_buckets) + len(self.decode_buckets)
-            torch._dynamo.config.cache_size_limit = cache_size_limit
-            torch._dynamo.config.accumulated_cache_size_limit = cache_size_limit * 8
+            torch._dynamo.config.cache_size_limit = max(cache_size_limit, 
+                                                        torch._dynamo.config.cache_size_limit)
+            torch._dynamo.config.accumulated_cache_size_limit = max(cache_size_limit * 8, 
+                                                                    torch._dynamo.config.accumulated_cache_size_limit)
         
         start_mem = HabanaMemoryProfiler.current_device_memory_usage()
         start_time = time.perf_counter()
